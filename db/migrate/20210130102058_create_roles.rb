69dec76f8042464e7bb433c23d5e36f6c9ec482d
manish anand <manish4uanand@gmail.com>
2021-01-30 17:38:12 +0530

    added role model

---
+++ db/migrate/20210130102058_create_roles.rb
@@ -0,0 +1,11 @@
+class CreateRoles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :roles do |t|
+      t.string :role_id
+      t.string :role_name, unique: true
+      t.string :role_code, unique: true
+
+      t.timestamps
+    end
+  end
+end
