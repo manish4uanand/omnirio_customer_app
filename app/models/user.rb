85a748fbc8ba1fe104a16a9a136a35d98c40b959
manish anand <manish4uanand@gmail.com>
2021-01-30 23:07:40 +0530

    implemented enum for gender for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum gender: { male: 0, female: 1, not_sure: 2, prefer_not_to_disclose: 3 }
 end
