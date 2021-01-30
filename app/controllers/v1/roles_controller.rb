f56f38a2097882198c727d5ec1b846c4d53d0380
manish anand <manish4uanand@gmail.com>
2021-01-30 17:47:32 +0530

    added roles controller and rack cors gem

---
+++ app/controllers/v1/roles_controller.rb
@@ -0,0 +1,21 @@
+class V1::RolesController < ApplicationController
+  def index
+    @roles = Role.all
+
+    render json: @roles, status: :ok
+  end
+
+  def create
+    @role = Role.new(role_params)
+    @role.save
+
+    render json: @role, status: :created
+  end
+
+  private
+
+  def role_params
+    params.require(:role).permit(:role_id, :role_code, :role_name)
+  end
+
+end
